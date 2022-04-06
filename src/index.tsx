30deca2774ffce4ef16b2384d07ece039cf3a50b
Alexander Gorbunov <lexgorbunov@gmail.com>
2022-04-06 13:46:30 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -186,7 +186,7 @@ const makeLogString = (
       type = 'TRACE'
       break
   }
-  return `📠 [${time} ${type}]: ▸ ${message}${error}\n`
+  return `📠 [${time} ${type}]: ▸ ${message} ${error}\n`
 }
 
 const pad = (value: number): string => {
